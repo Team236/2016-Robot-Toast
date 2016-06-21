22ce25eff23e295f01e73010bded89b889783f88
Sam Fuchs <samcfuchs@gmail.com>
2016-06-21 16:44:35 -0500

    Fixed path issues with gradle build

--- src/main/java/frc/team236/stronghold/RobotModule.java
+++ src/main/java/frc/team236/stronghold/RobotModule.java
@@ -1,4 +1,7 @@
-package frc.team236.2016-robot-toast;
+package frc.team236.stronghold;
+
+import frc.team236.ticktank.TickTank;
+import frc.team236.ticktank.Settings;
 
 import jaci.openrio.toast.lib.log.Logger;
 import jaci.openrio.toast.lib.module.IterativeModule;
@@ -6,6 +9,9 @@ import jaci.openrio.toast.lib.module.IterativeModule;
 public class RobotModule extends IterativeModule {
 
     public static Logger logger;
+
+    public static TickTank drive;
+    public static Settings driveConfig;
 
     @Override
     public String getModuleName() {
@@ -20,6 +26,8 @@ public class RobotModule extends IterativeModule {
     @Override
     public void robotInit() {
         logger = new Logger("2016-Robot-Toast", Logger.ATTR_DEFAULT);
+
+        driveConfig = new Settings();
         //TODO: Module Init
     }
 }
