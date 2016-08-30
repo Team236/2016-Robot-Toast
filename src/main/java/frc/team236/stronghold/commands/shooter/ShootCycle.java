a3b6c60e31f1bd1bd2bb84046b640e3931be180e
Sam Fuchs <samcfuchs@gmail.com>
2016-08-30 12:38:34 -0400

    Format new files

--- src/main/java/frc/team236/stronghold/commands/shooter/ShootCycle.java
+++ src/main/java/frc/team236/stronghold/commands/shooter/ShootCycle.java
@@ -4,9 +4,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team236.stronghold.commands.Wait;
 
 public class ShootCycle extends CommandGroup {
-    public ShootCycle() {
-	addSequential(new Shoot());
-	addSequential(new Wait(0.0D));
-	addSequential(new Cock());
-    }
+	public ShootCycle() {
+		addSequential(new Shoot());
+		addSequential(new Wait(0.0D));
+		addSequential(new Cock());
+	}
 }
